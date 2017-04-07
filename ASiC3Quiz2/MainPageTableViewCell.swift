3a6a4a04ae86587b5a50779e05200f9a667a4b34
athemer <athemer0520@gmail.com>
2017-04-07 10:16:29 +0800

    xib done

---
+++ ASiC3Quiz2/MainPageTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MainPageTableViewCell.swift
+//  ASiC3Quiz2
+//
+//  Created by 陳冠華 on 2017/4/7.
+//  Copyright © 2017年 my app. All rights reserved.
+//
+
+import UIKit
+
+class MainPageTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
